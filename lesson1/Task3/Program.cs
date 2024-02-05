4f7c6dfa4b62af30b1b164870d4a16db3bd26ea4
Vagid <vagidbedalov@gmail.com>
2024-02-05 19:30:22 +0300

    2commit

---
+++ lesson1/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задание 3
+void PrintArray(int[] array, int i)
+{
+    if(i < 0)
+    {
+        return;
+    }
+    if (i == array.Length)
+    {
+        return;
+    }
+    Console.Write(array[i]);
+    PrintArray(array, i - 1);
+}
+int[] array = {1, 2, 5, 0, 10, 34};
+PrintArray(array, 5);
