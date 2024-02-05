e16d132c8cd03bafb2b5245e02c90f50e0128eda
Vagid <vagidbedalov@gmail.com>
2024-02-05 19:26:32 +0300

    new1

---
+++ lesson1/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задание 1
+int m = 1;
+int n = 5;
+void nat(int m, int n)
+{
+    if (m > n)
+    return;
+    if (m % 2 == 0)
+    {
+        Console.Write($"{m}, ");
+    }
+    nat(m + 1, n);
+}
+nat(m, n);
